642355bde08b88092e594065f51981f7e392769c
vyanphan <vyan.n.phan@gmail.com>
2019-05-01 02:31:06 -0700

    display

--- main_wrapper.py
+++ main_wrapper.py
@@ -2,7 +2,7 @@ from html5lib import *
 from blockchain_utils import *
 import os
 
-
+TABLE_LABELS = ['Proof', 'Next Block', 'Update Body', 'Time']
 
 def append_update(blockchain, proof, update_file, private_mode):
     with open(update_file, "r") as rd_file:
@@ -32,18 +32,30 @@ def parse_chain(chain_folder, chain_head):
             next_block = block_file.readline()[:-1]
             b3 = block_file.readline()
             b4 = block_file.readline()
-        curr_block = b1 + b2 + next_block + "\n" + b3 + b4
+        curr_block = (b1, b2, next_block + "\n", b3, b4)
         blocks += [curr_block]
         next_block = chain_folder + next_block
     return blocks
 
-def display(chain_folder, chain_head):
+def display(output_file, chain_folder, chain_head):
     blocks = parse_chain(chain_folder, chain_head)
 
-    for b in blocks:
-        print(b)
+    with open(output_file, 'w+') as out_file:
+        out_file.write('<html><head><title>' + chain_folder + '</title>')
+        out_file.write('<link rel="stylesheet" href="format.css"></head>')
+        out_file.write('<body><h1 align="center">' + chain_folder + '</h1>')
+
+        for b in blocks:
+            out_file.write('<table>')
+            out_file.write('<tr><th colspan=2>' + b[0] + '</th></tr> <col width="20%"/><col width="80%"/>')
+            for i in range(0,4):    
+                out_file.write('<tr><td><b>' + TABLE_LABELS[i] + '</b></td><td>' + b[i+1] + '</td>')
+            out_file.write('</table>')
+            out_file.write('<br>')
+
+        out_file.write('</body></html>')
 
 
 
-display('newchain_1', 'newchain_1')
+display('newchain_1.html', 'newchain_1', 'newchain_1')
 
